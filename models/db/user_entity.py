b84cc66607372b8bb57fb9b459dd892178cc4272
alibahrami1376 <69414558+alibahrami1376@users.noreply.github.com>
2025-04-23 07:56:51 -0700

    Set up Alembic for database migrations and added new column to user_entity model

--- models/db/user_entity.py
+++ models/db/user_entity.py
@@ -10,8 +10,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 class UserEntity(Base):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4().hex))
     username: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email_address: Mapped[str] = mapped_column(String(255),default=None)
+    phone_number:Mapped[str]= mapped_column(String(50),default=None)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     tasks: Mapped[list["TaskEntity"]] = relationship(back_populates="user", cascade="all, delete-orphan")
